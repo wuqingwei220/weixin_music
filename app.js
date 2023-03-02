e4773256442b7a0dae1442f2969e51001596083d
coderwqw <coderwqw@qq.com>
2023-03-02 13:48:04 +0800

    x

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ App({
   onLaunch() {
     // 1.获取设备的信息
     wx.getSystemInfo({
+      
       success: (res) => {
         this.globalData.screenWidth = res.screenWidth
         this.globalData.screenHeight = res.screenHeight
